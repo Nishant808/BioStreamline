08546d82d1f5cefa52ae353d72245566eb905edf
Nishant Thalwal <nishantthalwal@gmail.com>
2024-12-23 12:45:39 +0530

    Update retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -2,13 +2,14 @@ import Bio
 import Bio.Entrez
 import os
 
+# Set your email for NCBI requests
+Bio.Entrez.email = 'nishantthalwal@gmail.com'
+
 # Define a global list for IDs
 ID = []
 
-Bio.Entrez.email = 'nishantthalwal@gmail.com'  # Set your email
-search_term = input("Enter The Search Term: ")  # Get the search term from the user
-  
-
+# Get the search term from the user
+search_term = input("Enter The Search Term: ")
 
 def ncbi_genbank():
     global ID  # Declare ID as a global variable to modify it inside the function
@@ -20,13 +21,13 @@ def ncbi_genbank():
     # Clear the global ID list to avoid appending to old results
     ID.clear()
     
-    # Populate the ID list
-    for i in search_results['IdList']:  # Iterate over the 'IdList' in the parsed dictionary
-        ID.append(i)  # Append each ID to the list
+    # Populate the ID list and write to ID.txt
+    with open('ID.txt', 'w') as id_file:
+        for i in search_results['IdList']:  # Iterate over the 'IdList' in the parsed dictionary
+            ID.append(i)  # Append each ID to the list
+            id_file.write(f"{i}\n")  # Write each ID to the file
 
     print(f"Search Results: {search_results}")
     print(f"IDs: {ID}")
-
-
 
 
