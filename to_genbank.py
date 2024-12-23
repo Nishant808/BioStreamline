6ddcd7dc38baaafc79bec00dba4a0f2d73642cdc
Nishant Thalwal <nishantthalwal@gmail.com>
2024-12-23 12:47:00 +0530

    Update to_genbank.py

--- to_genbank.py
+++ to_genbank.py
@@ -2,21 +2,24 @@ from retrieval import ID
 import os
 import Bio
 
+# Ensure Bio.Entrez.email is set
+Bio.Entrez.email = 'nishantthalwal@gmail.com'
+
+# Default folder name
+folder_path = "GenBankFiles"
+
+# Check if the folder exists; if not, create it
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
 # Define the save function
 def save_genbank():
-    # Ensure Bio.Entrez.email is set
-    Bio.Entrez.email = 'nishantthalwal@gmail.com'
-
-    # Default folder name
-    folder_path = "GenBankFiles"
-
-    # Check if the folder exists; if not, create it
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+    # Read IDs from the ID.txt file
+    with open('ID.txt', 'r') as id_file:
+        ids = [line.strip() for line in id_file.readlines()]
 
     # Fetch and save GenBank files
-    for i in ID:  # Loop through the list of IDs
+    for i in ids:  # Loop through the list of IDs
         # Fetch GenBank data
         with Bio.Entrez.efetch(db='nucleotide', id=i, rettype="gb", retmode="text") as handle:
             genbank_data = handle.read()
@@ -30,7 +33,3 @@ def save_genbank():
             file.write(genbank_data)
 
         print(f"Saved GenBank file for ID {i} to {file_path}")
-
-
-
-
